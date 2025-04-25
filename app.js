0f189510c111cf915c9308f088230dc54dffaa15
arpitasingh16 <arpitasingh.rajput16@gmail.com>
2025-04-26 02:49:36 +0530

    Main file

--- app.js
+++ app.js
@@ -3,11 +3,17 @@ const app = express();
 const bodyParser = require('body-parser');
 const route = require('./routes/route'); 
 const port = 3000;
-
+const path = require('path');
 // Middleware to parse JSON request body
 app.use(bodyParser.json());
 
 app.use('/', route);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/addSchool', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 app.use('/addSchool', route);  
 app.use('/listSchool', route); 
